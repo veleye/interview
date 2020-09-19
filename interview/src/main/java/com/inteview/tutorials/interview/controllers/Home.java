eb57b577250190c37d78a8aab0ab67d5fd5019ee
erkan obut <tbd.eobut@softtech.com.tr>
2020-09-19 20:49:45 +0300

    new controller

---
+++ interview/src/main/java/com/inteview/tutorials/interview/controllers/Home.java
@@ -0,0 +1,14 @@
+package com.inteview.tutorials.interview.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+    
+    @RequestMapping("/")
+    public String getHomePage(){
+        return "";
+    }
+
+}
